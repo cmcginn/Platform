76fe9e6572c9b841def7f447d0b8c8bb2ec98645
cmcginn <Urbana@000!0>
2015-09-16 13:37:27 -0400

    added angle

---
+++ Platform.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Platform.Web.Startup))]
+namespace Platform.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
